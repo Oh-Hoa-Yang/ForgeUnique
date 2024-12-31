3fb9a768d74fdc6a69fcae49d24281bf0865435c
Oh-Hoa-Yang <ohhaoyang1@gmail.com>
2024-12-31 22:41:21 +0800

    testing online manage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
       login: '/login',
       callback: '/confirm',
       include: undefined,
-      exclude: ["/register", "/forgot_password","/reset-password","/finishregistered"], 
+      exclude: ["/register", "/forgot_password","/reset-password","/finishregistered","/forgotpassword_notice"], 
       cookieRedirect: false
     },
     clientOptions: {
---
+++ pages/forgotpassword_notice.vue
@@ -0,0 +1,48 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding custom-background">
+      <ion-card>
+        <div>
+          <ion-button class="custom-button" disabled="true"><b>NOTICE</b></ion-button>
+        </div>
+        <p style="padding: 20px; text-align: start;">To make sure you are more securely manage your password, open the link below at the <b>BROWSER / WEBSITE :</b><br></p>
+        <p style="text-align: center;"><b>https://forgeunique.vercel.app/forgot_password</b></p>
+      </ion-card>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup>
+import { useAppToast } from '~/composables/useAppToast';
+const { toastError, toastSuccess } = useAppToast();
+const router = useRouter();
+const supabase = useSupabaseClient();
+
+</script>
+
+<style scoped>
+.center-img,
+ion-item,
+ion-button {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  padding: 20px;
+}
+
+.custom-background {
+  --background: #FFEDF5;
+}
+
+.custom-button {
+  --background: #f7537c;
+  --background-activated: #ff546e;
+  --background-focused: #ffadb9;
+  --background-hover: #ffadb9;
+  --background-pressed: #ffadb9;
+  font-weight: bold;
+  --color: black;
+  font-size: 20px;
+}
+</style>
