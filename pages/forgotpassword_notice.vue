983fea23fa2e5ee423ca542ce9aba4bc76ed0cc0
Oh-Hoa-Yang <ohhaoyang1@gmail.com>
2025-01-01 00:29:50 +0800

    forgotpassword_notice UI improvement only

--- pages/forgotpassword_notice.vue
+++ pages/forgotpassword_notice.vue
@@ -5,8 +5,10 @@
         <div>
           <ion-button class="custom-button" disabled="true"><b>NOTICE</b></ion-button>
         </div>
-        <p style="padding: 20px; text-align: start;">To make sure you are more securely manage your password, open the link below at the <b>BROWSER / WEBSITE :</b><br></p>
+        <p style="padding: 20px; text-align: start;">To make sure you are more securely manage your password, open the
+          link below at the <b>BROWSER / WEBSITE :</b><br></p>
         <p style="text-align: center;"><b>https://forgeunique.vercel.app/forgot_password</b></p>
+        <ion-button class="nav-button" router-link="/login">Back To Login</ion-button>
       </ion-card>
     </ion-content>
   </ion-page>
@@ -44,5 +46,14 @@ ion-button {
   font-weight: bold;
   --color: black;
   font-size: 20px;
+}
+
+.nav-button {
+  --background: #FFC2D1;
+  --background-activated: #ffadb9;
+  --background-focused: #ffadb9;
+  --background-hover: #ffadb9;
+  --background-pressed: #ffadb9;
+  --color: black;
 }
 </style>
